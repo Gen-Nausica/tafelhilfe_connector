eb87f7866089a19cd7edafd95e3b04dfd8b21e96
jpfeiffer <jenny.pfeiffer@spheos.com>
2020-04-04 18:10:48 +0200

    servlet takes json-data and creates object

--- src/main/java/Connector.java
+++ src/main/java/Connector.java
@@ -1,4 +1,6 @@
 import objects.Postobjekt;
+import org.json.HTTP;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import javax.servlet.ServletException;
@@ -6,20 +8,25 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
 
 
 @WebServlet(name = "Connector")
 public class Connector extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        if(request != null){
-            JSONObject json = new JSONObject(request);
-            Postobjekt po = new Postobjekt(json);
+        //System.out.println(request);
+        StringBuffer jb = new StringBuffer();
+        String line = null;
+        JSONObject jsonObject = null;
+        Postobjekt po = new Postobjekt(request.getParameter("name"),request.getParameter("address"), request.getParameter("plz"), request.getParameter("ort"), request.getParameter("anpsrechpartner"), request.getParameter("tel"), request.getParameter("fax"), request.getParameter("mail"), request.getParameter("url"), request.getParameter("kontaktart"), request.getParameter("option1"), request.getParameter("option2"), request.getParameter("option3"), request.getParameter("option4"), request.getParameter("kommentar"));
+       if(po != null){
             if(po.verify()){
                 po.postQuarantaeneHelden();
                 po.postWirHelfen();
             }
         }
+       response.
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
--- src/main/java/objects/Postobjekt.java
+++ src/main/java/objects/Postobjekt.java
@@ -5,10 +5,39 @@ import org.json.JSONObject;
 
 public class Postobjekt {
 
-    private JSONObject data;
+    private String name;
+    private String address;
+    private String plz;
+    private String ort;
+    private String ansprechpartner;
+    private String tel;
+    private String fax;
+    private String mail;
+    private String url;
+    private String kontaktart;
+    private String option1;
+    private String option2;
+    private String option3;
+    private String option4;
+    private String kommentar;
 
-    public Postobjekt(JSONObject request){
-        this.data = request;
+
+    public Postobjekt(String name, String address, String plz, String ort, String ansprechpartner, String tel, String fax, String mail, String url, String kontaktart, String option1, String option2, String option3, String option4, String kommentar){
+        this.name = name;
+        this.address = address;
+        this.plz = plz;
+        this.ort = ort;
+        this.ansprechpartner = ansprechpartner;
+        this.tel = tel;
+        this.fax = fax;
+        this.mail = mail;
+        this.url = url;
+        this.kontaktart = kontaktart;
+        this.option1 = option1;
+        this.option2 = option2;
+        this.option3 = option3;
+        this.option4 = option4;
+        this.kommentar = kommentar;
     }
 
     public boolean postQuarantaeneHelden(){
